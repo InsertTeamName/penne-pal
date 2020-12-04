b0c21bb1b8ecf8b46503da1ca25fdc4f733a5c28
Brynna <68093791+bwatring@users.noreply.github.com>
2020-12-03 20:13:59 -0600

    Update ShoppingList.java

--- src/application/model/ShoppingList.java
+++ src/application/model/ShoppingList.java
@@ -11,16 +11,19 @@ public class ShoppingList {
 		
 		return null;
 	}
-	
+	// @author Brynna watring, Christina Zhang, Zain Momin, Steven Luong, Chris Rademacher 
 	public List<Item> getItems() {
 		return items;
 	}
+	// @param a list of items from the user input 
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
+	// @return string listName that contains list of names 
 	public String getListName() {
 		return listName;
 	}
+	// @param a string of list names 
 	public void setListName(String listName) {
 		this.listName = listName;
 	}
